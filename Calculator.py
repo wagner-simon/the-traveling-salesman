0a2d85ce094a002c2726b1bac11e7b33f9d62c97
Axdecces <axdecces@gmail.com>
2017-09-19 15:31:33 +0200

    cleaned up code, loading bar is bugged and currently not working

--- Calculator.py
+++ Calculator.py
@@ -6,22 +6,33 @@ from Point import Point
 from util import RANDOM, PERMUTATION
 
 
-class Calculator():
+class Calculator:
     def __init__(self, game):
         self.game = game
-
         self.width = self.game.width
         self.height = self.game.height
 
+        self.set_thread_states()
+
+        self.set_counters()
+
+        self.set_points()
+
+        self.algorithm = RANDOM
+        self.selected_algorithm = self.algorithm
+
+    def set_thread_states(self):
         self.thread_running = False
         self.thread_stop = False
         self.thread_finished = False
 
-        self.algorithm = RANDOM
-        self.selected_algorithm = self.algorithm
+    def set_counters(self):
         self.percentage = 0
+        self.current_iteration = 0
         self.amount_of_iterations = 1
+        self.shortest_distance = 0
 
+    def set_points(self):
         self.shortest_path = []
         self.random_points = []
 
@@ -29,64 +40,76 @@ class Calculator():
         self.rangeY = (0, self.height)
         self.amount_of_points = 9
 
-        self.current_iteration = 0
-
-        self.shortest_distance = 0
-
-
     def start_thread(self):
-        self.thread = threading.Thread(target=Calculator.generate_random_points, args=(self,))
+        self.thread = threading.Thread(target=Calculator.reset_thread_variables, args=(self,))
         self.thread.daemon = True
         self.thread.start()
 
-    def generate_random_points(self):
+    def reset_thread_variables(self):
         self.thread_running = True
         self.thread_finished = False
-        self.shortest_distance = 0
-        self.shortest_path = []
-        self.random_points = []
+
+        self.set_counters()
+
+        self.set_points()
+
+        self.generate_random_points()
+
+        self.amount_of_iterations = math.factorial(self.amount_of_points)
+
+        self.get_distance()
+
+    def generate_random_points(self):
         while len(self.random_points) < self.amount_of_points:
             x = random.randrange(*self.rangeX)
             y = random.randrange(*self.rangeY)
             self.random_points.append(Point(x, y))
-        self.amount_of_iterations = math.factorial(self.amount_of_points)
-        self.get_distance()
 
     def get_distance(self):
-        if self.algorithm == PERMUTATION:
-            permutations = itertools.permutations(self.random_points)
+        self.set_permutations()
         i = 0
-        while True:
+        while self.thread_running:
             if self.thread_stop:
-                self.thread_stop = False
+                self.stop_thread()
+            self.current_iteration = i
+            self.get_new_path()
+            self.calculate_shortest_distance()
+            i += 1
+
+    def stop_thread(self):
+        self.thread_stop = False
+        self.thread_running = False
+
+    def set_permutations(self):
+        if self.algorithm == RANDOM:
+            self.permutations = None
+        if self.algorithm == PERMUTATION:
+            self.permutations = itertools.permutations(self.random_points)
+
+    def get_new_path(self):
+        if self.algorithm == RANDOM:
+            self.current_path = random.sample(self.random_points, len(self.random_points))
+        if self.algorithm == PERMUTATION:
+            try:
+                self.current_path = next(self.permutations)
+            except StopIteration:
+                self.thread_finished = True
                 self.thread_running = False
                 return
+            self.percentage = float(self.current_iteration + 1) / float(self.amount_of_iterations) * 100
 
-            self.current_iteration = i
+    def calculate_shortest_distance(self):
+        distance = 0
+        for index, point in enumerate(self.current_path):
+            if self.thread_stop:
+                self.stop_thread()
+            try:
+                distance += point.distance_to(self.current_path[index + 1])
+            except IndexError:
+                pass
+        self.set_shortest_distance(distance)
 
-            if self.algorithm == RANDOM:
-                current_path = random.sample(self.random_points, len(self.random_points))
-            if self.algorithm == PERMUTATION:
-                try:
-                    current_path = next(permutations)
-                except StopIteration:
-                    self.thread_finished = True
-                    self.thread_running = False
-                    #self.screenshot_requested = True
-                    return
-                self.percentage = float(self.current_iteration + 1) / float(self.amount_of_iterations) * 100
-
-            distance = 0
-            for index, point in enumerate(current_path):
-                if self.thread_stop:
-                    self.thread_stop = False
-                    self.thread_running = False
-                    return
-                try:
-                    distance += point.distance_to(current_path[index+1])
-                except IndexError:
-                    pass
-            if distance < self.shortest_distance or self.shortest_distance == 0:
-                self.shortest_distance = distance
-                self.shortest_path = current_path
-            i += 1
+    def set_shortest_distance(self, distance):
+        if distance < self.shortest_distance or self.shortest_distance == 0:
+            self.shortest_distance = distance
+            self.shortest_path = self.current_path
--- Renderer.py
+++ Renderer.py
@@ -1,9 +1,9 @@
 import pygame
-import time
 import math
 from util import RANDOM, PERMUTATION, ALGORITHM_NAMES
 
-class Renderer():
+
+class Renderer:
     def __init__(self, game):
         self.game = game
         self.background_color = 200, 200, 200
@@ -13,33 +13,84 @@ class Renderer():
         
         self.font = pygame.font.Font('assets/fonts/joystix monospace.ttf', 30)
 
-
     def draw(self):
         self.game.screen.fill(self.background_color)
         self.draw_shortest_path()
-        for point in self.game.calculator.random_points:
-            pygame.draw.rect(self.game.screen, (0, 0, 0), [point.x - math.floor(self.marker_size / 2), point.y - math.floor(self.marker_size / 2), self.marker_size, self.marker_size])
+        self.draw_points()
 
-        text_distance = self.font.render(str(int(math.floor(self.game.calculator.shortest_distance))), False, (255, 0, 0))
+        printing_distance = str(int(math.floor(self.game.calculator.shortest_distance)))
+        text_distance = self.font.render(printing_distance, False, (255, 0, 0))
         self.game.screen.blit(text_distance, (self.padding, self.padding))
-
-        if self.game.calculator.algorithm == RANDOM:
-            text_width, text_height = self.font.size(str(self.game.calculator.current_iteration))
-            text_iterations = self.font.render(str(self.game.calculator.current_iteration), False, (255, 0, 0))
-        if self.game.calculator.algorithm == PERMUTATION:
-            text_width, text_height = self.font.size(str(round(self.game.calculator.percentage, 2)) + "%")
-            text_iterations = self.font.render(str(round(self.game.calculator.percentage, 2)) + "%", False, (255, 0, 0))
-            pygame.draw.rect(self.game.screen, (255, 0, 0), [0, self.game.height - self.loading_bar_thickness, self.game.calculator.percentage / 100 * self.game.width, self.loading_bar_thickness])
-        self.game.screen.blit(text_iterations, (self.game.width - text_width - self.padding, self.game.height - text_height - self.padding))
-
-        text_algorithm_type = self.font.render(ALGORITHM_NAMES[self.game.calculator.algorithm], False, (255, 0, 0))
-        text_type_width, text_type_height = self.font.size(ALGORITHM_NAMES[self.game.calculator.algorithm])
-        self.game.screen.blit(text_algorithm_type, (self.game.width - text_type_width - self.padding, self.padding))
+        self.draw_indicators()
+        self.draw_algorithm_font()
 
     def draw_shortest_path(self):
         for index, point in enumerate(self.game.calculator.shortest_path):
             try:
-                pygame.draw.line(self.game.screen, (100, 100, 100), (point.x, point.y), (self.game.calculator.shortest_path[index+1].x, self.game.calculator.shortest_path[index+1].y), 2)
+                pygame.draw.line(
+                    self.game.screen,
+                     (100, 100, 100),
+                     (point.x, point.y),
+                     (self.game.calculator.shortest_path[index+1].x,
+                      self.game.calculator.shortest_path[index+1].y),
+                     2
+                )
             except IndexError:
                 pass
 
+    def draw_points(self):
+        for point in self.game.calculator.random_points:
+            draw_rect = \
+                [point.x - math.floor(self.marker_size / 2),
+                 point.y - math.floor(self.marker_size / 2),
+                 self.marker_size,
+                 self.marker_size]
+            pygame.draw.rect(self.game.screen, (0, 0, 0), draw_rect)
+
+    def draw_indicators(self):
+        if self.game.calculator.algorithm == RANDOM:
+            self.draw_RANDOM_text()
+        if self.game.calculator.algorithm == PERMUTATION:
+            self.draw_PERMUTATION_text()
+            #self.draw_loading_bar()
+
+
+
+    def draw_RANDOM_text(self):
+        text_width, text_height = self.font.size(str(self.game.calculator.current_iteration))
+        text_iterations = self.font.render(str(self.game.calculator.current_iteration), False, (255, 0, 0))
+        self.game.screen.blit(text_iterations,
+                              (self.game.width - text_width - self.padding,
+                            self.game.height - text_height - self.padding)
+                              )
+
+    def draw_PERMUTATION_text(self):
+        text_width, text_height = self.font.size(str(round(self.game.calculator.percentage, 2)) + "%")
+        text_iterations = self.font.render(
+            str(round(self.game.calculator.percentage, 2)) + "%",
+            False,
+            (255, 0, 0))
+        self.game.screen.blit(
+            source=text_iterations,
+            dest=(
+                self.game.width - text_width - self.padding,
+                self.game.height - text_height - self.padding
+            )
+        )
+
+
+    def draw_loading_bar(self):
+        loading_bar_progress = self.game.calculator.percentage / 100 * self.game.width
+        draw_rect = (
+            0,
+            self.game.height - self.loading_bar_thickness,
+            loading_bar_progress,
+            self.loading_bar_thickness
+        )
+
+        pygame.draw.rect(Surface=self.game.screen, color=(255, 0, 0), Rect=draw_rect)
+
+    def draw_algorithm_font(self):
+        text_algorithm_type = self.font.render(ALGORITHM_NAMES[self.game.calculator.algorithm], False, (255, 0, 0))
+        text_type_width, text_type_height = self.font.size(ALGORITHM_NAMES[self.game.calculator.algorithm])
+        self.game.screen.blit(text_algorithm_type, (self.game.width - text_type_width - self.padding, self.padding))
